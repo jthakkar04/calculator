31cb415f44b9101d277a61be9515d1163f940412
Jagat <Jagat@purdue.edu>
2018-11-11 10:27:22 -0600

    first commit

---
+++ watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  watch Extension
+//
+//  Created by Jagat Thakkar on 11/10/18.
+//  Copyright © 2018 Jagat Thakkar. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
